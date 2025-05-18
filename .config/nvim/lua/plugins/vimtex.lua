5bf49f62d1171f883e2a318570fec41051c8fe94
fraziema <107295620+fraziema@users.noreply.github.com>
2025-05-18 04:59:51 -0400

    add iron, vimtex changes

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,5 +4,7 @@ return {
 	init = function()
     -- put vim.g.vimtex_* settings here
 	vim.g.vimtex_view_method = 'zathura'
+	vim.g.vimtex_quickfix_autoclose_after_keystrokes = 2
+	vim.g.vimtex_quickfix_open_on_warning = 0
   end
 }
