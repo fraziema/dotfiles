908abb285b3f7604812b70e4726d52e178fc4fac
fraziema <107295620+fraziema@users.noreply.github.com>
2025-06-23 12:26:37 -0400

    installed luasnip et.al.

---
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,9 @@
+
+return {
+    "L3MON4D3/LuaSnip",
+	lazy = true,
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)  '
+
+	-- install jsregexp (optional!).  
+	build = "make install_jsregexp"
+}
