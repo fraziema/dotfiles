97e087625b7916fadcc80ce160592626749a0a19
fraziema <107295620+fraziema@users.noreply.github.com>
2025-07-11 08:08:15 -0400

    color deez

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -2,25 +2,59 @@ return{
 	{
 		"NLKNguyen/papercolor-theme",     priority = 1000,
 	},
+
 	{
 		"ptdewey/darkearth-nvim",     priority = 1000,
 	},
+	
+	-- when the colorscheme has a setup, you need to include it like this
+	-- config = function()
+	-- 	require("<COLORSCHEME>").setup({
+		-- 	example setting variables/flags here
+	-- 		no_italic = true,
+	-- 		transparent_background = true,
+	-- 		styles = {
+	-- 			comments = { NONE },
+	-- 		}
+	-- 		...
+	-- 	})
+	-- end
 
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		opts = {
-			transparent_background = true,
-		}
+		config = function()
+			require("catppuccin").setup({
+				no_italic = true,
+				transparent_background = true,
+				styles = {
+					comments = { NONE },
+				}
+			})
+		end
 	},
 
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
-		opts = { }
+		opts = { },
+		config = function()
+			require("tokyonight").setup({
+				transparent = false,
+				styles = {
+					comments = { italic = false },
+					keywords = { italic = false },
+					functions = { bold = true },
+					variables = {},
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+			})
+		end
 	},
-		
+
 	{   
 		"olimorris/onedarkpro.nvim",   
 		priority = 1000, 
@@ -28,18 +62,31 @@ return{
 
 	{
 		"Koalhack/darcubox-nvim",
+		config = function()
+			require('darcubox').setup({
+				options = {
+					transparent = true,
+					styles = {
+						comments = {  }, 
+						functions = { bold = true }, 
+						keywords = { bold = true },
+						types = { bold = true }, 
+					}
+				}
+			})
+		end
 	},
 
 	{
-      'sainnhe/sonokai',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-        vim.g.sonokai_enable_italic = false
-      end
-    },
+		'sainnhe/sonokai',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.sonokai_enable_italic = false
+		end
+	},
 
 	{
 		'sainnhe/edge',
@@ -69,7 +116,19 @@ return{
 		config = true,
 		opts = {
 			transparent = true,
-		}
+		},
+		config = function()
+			require("gruvbox").setup({
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				transparent_mode = true,
+			})
+		end
 
 	}
 
