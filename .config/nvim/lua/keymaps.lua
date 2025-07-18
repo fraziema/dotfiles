3c8dbd43ade9bc6aee8af94d96e95c14765eaced
fraziema <107295620+fraziema@users.noreply.github.com>
2025-07-18 03:56:35 -0400

    tmux, nvim, picom deez

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,12 +6,20 @@ vim.keymap.set("n", "<leader>sb", function ()
 	vim.cmd.Telescope("buffers")
 	end , { desc = "[S]earch [B]uffers (remember double leader)" })
 
+	-- Navigate buffers
+vim.keymap.set("n", "<Right>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<Left>", ":bprevious<CR>", opts)
+
 -- Keybinds to make split navigation easi, { desc = "Move focus to the left window" })
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", 
+	{ desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", 
+	{ desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", 
+	{ desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", 
+	{ desc = "Move focus to the upper window" })
 
 -- open directory where file is? (open pwd -- project view)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "open pwd netrw" })
@@ -22,6 +30,20 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "center cursor" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "center cursor" })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+-- becuase centering and scrolloff makes H and L useless, remap to 
+-- beginning and end of text in line
+vim.keymap.set({ "n", "x", "o" }, "H", "^", opts)
+vim.keymap.set({ "n", "x", "o" }, "L", "g_", opts)
+
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Move selected line / block of text in visual mode
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 
 -- paste over text, and keep old text in paste buffer 
